8cf349a97a856a3216a5fa1472aaa0745ece2cb8
bunker9dev <bunker9dev@gmail.com>
2025-04-22 22:42:59 -0500

    js de galeria

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,19 @@
-alert("Hola ya llegue")
+document.addEventListener ('DOMContentLoaded', function() {
+    crearGaleria()
+
+})
+
+function   crearGaleria() {
+    const galeria = document.querySelector('.galeria-imagenes')
+
+    for (let i = 1; i <= 16; i++) {
+        const imagen = document.createElement('IMG')
+        imagen.src =  `src/img/gallery/full/${i}.jpg`
+        imagen.alt =  `Imagen Galeria`
+
+
+        galeria.appendChild(imagen)
+
+    }
+
+}
