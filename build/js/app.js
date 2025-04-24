c71628c6ddf38f1251a9c0b296f850cbf1a62b57
bunker9dev <bunker9dev@gmail.com>
2025-04-24 10:56:42 -0500

    galeria

--- build/js/app.js
+++ build/js/app.js
@@ -11,9 +11,53 @@ function   crearGaleria() {
         imagen.src =  `src/img/gallery/full/${i}.jpg`
         imagen.alt =  `Imagen Galeria`
 
+        //Event Handler 
+        
+        imagen.onclick = function () {
+           mostrarImagen( i )
+        }
 
         galeria.appendChild(imagen)
 
     }
 
+}
+
+function mostrarImagen( i ) {
+    const imagen = document.createElement('IMG')
+        imagen.src =  `src/img/gallery/full/${i}.jpg`
+        imagen.alt =  `Imagen Galeria`
+
+      // General ventana mmodal
+    const modal = document.createElement("DIV")
+    modal.classList.add("modal")
+    modal.onclick = cerrarModal
+    modal.appendChild(imagen)
+
+
+    // Agregar al HTML
+    const body = document.querySelector('body')
+    body.classList.add('overFlow-hidden')
+    body.appendChild(modal)
+
+    // console.log( modal);
+
+}
+
+function cerrarModal(){
+
+    const modal  = document.querySelector('.modal')
+    modal.classList.add ('fadeOut')
+
+    setTimeout(() => {
+        modal?.remove()
+
+        const body = document.querySelector('body')
+        body.classList.remove('overFlow-hidden')
+
+
+    }, 500);
+
+
+   
 }
